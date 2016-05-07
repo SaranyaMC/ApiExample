d5181377abcde0e6226c3c4a303ee1ad60333fc1
vignesh <vignesh0177@gmail.com>
2016-05-07 15:10:00 +0530

    Initial Commit

---
+++ ApiExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ApiExample
+//
+//  Created by vignesh on 07/05/16.
+//  Copyright © 2016 Home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ApiExample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ApiExample
+//
+//  Created by vignesh on 07/05/16.
+//  Copyright © 2016 Home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
